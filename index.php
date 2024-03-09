472f3151e9f9a88890711d5532a894db78b29c02
MhL5 <sapumr@gmail.com>
2024-03-09 19:08:16 +0330

    fix styles and rendering text

--- index.php
+++ index.php
@@ -85,7 +85,7 @@ timeCheck();
 
 <body>
     <main class="main" id="particles-js">
-        <div>
+        <div class="form-text-container">
             <form action="http://localhost/" method="post" class="form">
                 <div class="title">SPAMMER 69</div>
                 <label for="data"> Number Spam : </label>
@@ -106,17 +106,23 @@ timeCheck();
 
                 <button class="submit-btn">Submit</button>
             </form>
-            <!-- <input type="text" readonly>
+
+            <p>
             <?php
             if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 include "./backend/library/function.php";
                 echo spammer69($_POST['number'], $_POST['text'], $_POST['emoji_left']);
             }
             ?>
-            </input> -->
-            <div class="social-media">
-                <a href="Https://t.me/SAS3PAIN3" target="self">My Telegram</a>
-            </div>
+            </p>
+
+        </div>
+        <div class="social-media">
+            <a href="Https://t.me/SAS3PAIN3" target="self">My Telegram 
+            <span>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M248 8C111 8 0 119 0 256S111 504 248 504 496 393 496 256 385 8 248 8zM363 176.7c-3.7 39.2-19.9 134.4-28.1 178.3-3.5 18.6-10.3 24.8-16.9 25.4-14.4 1.3-25.3-9.5-39.3-18.7-21.8-14.3-34.2-23.2-55.3-37.2-24.5-16.1-8.6-25 5.3-39.5 3.7-3.8 67.1-61.5 68.3-66.7 .2-.7 .3-3.1-1.2-4.4s-3.6-.8-5.1-.5q-3.3 .7-104.6 69.1-14.8 10.2-26.9 9.9c-8.9-.2-25.9-5-38.6-9.1-15.5-5-27.9-7.7-26.8-16.3q.8-6.7 18.5-13.7 108.4-47.2 144.6-62.3c68.9-28.6 83.2-33.6 92.5-33.8 2.1 0 6.6 .5 9.6 2.9a10.5 10.5 0 0 1 3.5 6.7A43.8 43.8 0 0 1 363 176.7z"/></svg>
+            </span>
+            </a>
         </div>
     </main>
     <script>
